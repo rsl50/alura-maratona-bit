23c0d9e2dc2b6703eca52361a056d9fbb05f7a0b
Robson Lima <rsl.robson@gmail.com>
2020-01-08 14:45:43 -0300

    Commit Inicial com solução para o desafio BIT do SPOJ

---
+++ bit.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+// considerando 5 dígitos + 0x0A + 0x00
+#define LINE_SIZE	7
+
+int main () {
+	FILE *fp;	
+	fp = freopen("input1.txt", "r", stdin); 
+	
+    char buf[LINE_SIZE];    
+    memset(buf, 0, LINE_SIZE);
+	
+	if (fp == NULL) {
+    	printf("Erro ao abrir arquivo");
+    	return(-1);
+    }
+    else {
+        int notas[] = {50, 10, 5, 1};
+        int t = 1;
+		
+		while(fgets(buf, sizeof(buf), fp) != NULL) {
+			
+	        int valor = strtol(buf, NULL, 10);
+	        
+			if (valor == 0) break;
+	        	
+			int i, quantidadeDeNotas = 0;
+			
+			printf("Teste %d\n", t++);
+			
+	        for (i = 0; i < sizeof(notas)/sizeof(notas[0]); i++) {
+	        	quantidadeDeNotas = valor / notas[i];
+	        	valor = valor % notas[i];		        	
+	        	printf("%d ", quantidadeDeNotas);
+	        }
+	        
+	        printf("\n\n");
+    	}
+    }
+
+    fclose(fp);
+
+    return (0);
+}
+
+
